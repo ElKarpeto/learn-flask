bb407cf664ae257be1c76ddaa7f4f2af2793bf46
al mubarok <daffa1mubarok@gmail.com>
2023-05-19 00:39:39 +0700

    belajar hari 1

--- belajar1.py
+++ belajar1.py
@@ -1,1 +1,19 @@
-print('hello world')
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+# ini itu URL nya, karna kita nge-set dia "/", maka ini tu home page nya (PATH nya masih di homepage)
+def hello_world():
+    return 'hello semua nya'
+
+"""
+untuk run code flask kita, kita menggunakan code :
+
+flask --app (nama file) run
+
+"""
+
+if __name__ == '__main__' :
+    app.run(host='0.0.0.0', debug=True)
+
